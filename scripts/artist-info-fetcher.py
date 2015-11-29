83c868d33200864d5b01551b166c070c96449844
Georg Eschbacher <geschbacher.mmt-m2014@fh-salzburg.ac.at>
2015-11-29 15:56:36 +0100

    artist info fetcher updated

--- scripts/artist-info-fetcher.py
+++ scripts/artist-info-fetcher.py
@@ -4,10 +4,38 @@ __author__ = "Eschbacher - Kratzer - Scharfetter"
 import os
 import sys
 import helpers
+import Queue
+import threading
+import json
+import urllib
+import urllib2
 
 WIKIPEDIA_URL_SS = "http://en.wikipedia.org/wiki/Special:Search/"
 THEAUDIODB_URL = "http://www.theaudiodb.com/artist/"
 DISCOGS_URL = "http://www.discogs.com/artist/"
+
+fetcher = [WIKIPEDIA_URL_SS, THEAUDIODB_URL, DISCOGS_URL]
+
+def getUrl(q, url):
+    print url
+    q.put(urllib2.urlopen(url).read())
+
+
+def getArtistInfo(artist):
+    contentMerged = []
+    q = Queue.Queue()
+
+    for j, baseUrl in enumerate(fetcher):
+        url = baseUrl + urllib2.quote(artist)
+
+        t = threading.Thread(target=getUrl, args = (q,url))
+        t.daemon = True
+        t.start()
+
+    for k in range(0, len(fetcher)):
+        contentMerged.append(q.get())
+
+    return contentMerged
 
 # Main program
 if __name__ == '__main__':
@@ -19,16 +47,14 @@ if __name__ == '__main__':
 
     artists = helpers.readFile(pathToUniqueArtists)
 
-    for i in range(0, len(artists)):
-        htmlFn = pathToArtistInfo + "/" + str(i) + ".html"
+    for i in range(1, len(artists)):
+        print "Fetching artist " + str(i)
+        htmlFn = pathToArtistInfo + str(i) + ".html"
         if os.path.exists(htmlFn):
             print "File already fetched: " + htmlFn
             continue
 
-        htmlContent = helpers.fetchWebPage(WIKIPEDIA_URL_SS, artists[i])
-        htmlContent += helpers.fetchWebPage(THEAUDIODB_URL, artists[i])
-        htmlContent += helpers.fetchWebPage(DISCOGS_URL, artists[i])
-
+        htmlContent = getArtistInfo(artists[i])
         print "Storing content to " + htmlFn
         with open(htmlFn, 'w') as f:
-            f.write(htmlContent)
+            f.write("".join(htmlContent))
